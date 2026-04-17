7d235f98281b85dded53c9c0066e1030449dab0d
Prashanth9014 <botteprashanthyadav@gmail.com>
2026-04-17 18:05:24 +0530

    finaltogithiub

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,35 +2,35 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3003,
-    strictPort: true,
-    cors: true,
-   allowedHosts: true,
-    proxy:{
-      '/api': {
-        target: 'http://192.168.29.177:5050',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        configure: (proxy, options) => {
-          proxy.on('error', (err, req, res) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
+    plugins: [react()],
+    server: {
+        host: '0.0.0.0',
+        port: 3003,
+        strictPort: true,
+        cors: true,
+        allowedHosts: true,
+        proxy: {
+            '/api': {
+                target: 'http://192.168.29.114:5050',
+                changeOrigin: true,
+                secure: false,
+                ws: true,
+                configure: (proxy, options) => {
+                    proxy.on('error', (err, req, res) => {
+                        console.log('proxy error', err);
+                    });
+                    proxy.on('proxyReq', (proxyReq, req, res) => {
+                        console.log('Sending Request to the Target:', req.method, req.url);
+                    });
+                    proxy.on('proxyRes', (proxyRes, req, res) => {
+                        console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+                    });
+                },
+            },
         },
-      },
     },
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 3003,
-  },
+    preview: {
+        host: '0.0.0.0',
+        port: 3003,
+    },
 })
